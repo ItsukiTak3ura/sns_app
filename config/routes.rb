373ad7a8f37c258d6991c14d21a70fc840ed0d0e
名前 <>
2023-11-08 12:50:26 +0000

    2-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # PostsController
+   get 'posts/new', to: 'posts#new', as: 'new_post'
+
+  # TopicsController
+   get 'topics/new', to: 'topics#new', as: 'new_post'
+   get 'topics/edit', to: 'topics#edit', as: 'edit_post'
+  
 end
