d1337f63cc83758604a790465a5be3902a776b83
名前 <>
2023-11-28 11:20:54 +0000

    2-7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,18 @@
+# app/controllers/posts_controller.rb
+
 class PostsController < ApplicationController
   def new
     render :new
+  end
+
+  def create
+    redirect_to new_post_path
+  end
+
+  def index
+    # 必要な処理を実装
+    # 例: データベースから投稿を取得する
+    # @posts = Post.all
+    render :index
   end
 end
